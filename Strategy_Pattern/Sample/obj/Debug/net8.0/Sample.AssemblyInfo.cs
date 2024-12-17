0b60883dc2b5ea8bc681c3cc94d83fdc33415f26
Sujeet Kumar <sujeetkumar200075@gmail.com>
2024-12-18 01:04:17 +0530

    Sample (Vehicle) using Strategy Pattern

--- Strategy_Pattern/Sample/obj/Debug/net8.0/Sample.AssemblyInfo.cs
+++ Strategy_Pattern/Sample/obj/Debug/net8.0/Sample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dae6f70dad18c1848782ac63fc0d4018cd3282b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8d5bc81db881d2fd8822c39976a488ca88746c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
